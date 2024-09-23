38c66a114b2df90b4978e782a65fde00ae7169a9
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2024-09-23 13:24:54 -0300

    Update SecurityConfig.java

--- src/main/java/com/agendasenac/SecurityConfig.java
+++ src/main/java/com/agendasenac/SecurityConfig.java
@@ -1,3 +1,4 @@
+package com.agendasenac;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,7 +9,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.agendasenac.components.JwtAuthenticationFilter;
+
+import com.agendasenac.componets.JwtAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
