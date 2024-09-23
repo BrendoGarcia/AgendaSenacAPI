f92d9687c2f5ce064015c1c7d6cd2f572ed733f9
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2024-09-23 10:12:40 -0300

    Update SecurityConfig.java

--- src/main/java/com/agendasenac/SecurityConfig.java
+++ src/main/java/com/agendasenac/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.agendasenac;
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
-
 import com.agendasenac.componets.JwtAuthenticationFilter;
 
 @Component
@@ -27,20 +25,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+            .cors()  // Habilita CORS no Spring Security
+            .and()
             .csrf(csrf -> csrf.disable())  // Desabilita o CSRF
             .authorizeHttpRequests(auth -> auth
-                // Endpoints públicos
-                .requestMatchers("/login", "/register","/user/**").permitAll()
-                // Endpoints protegidos por roles (usando hasAuthority)
+                .requestMatchers("/login", "/register/**", "/user/**").permitAll()
                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
                 .requestMatchers("/professor/**").hasAuthority("PROFESSOR")
                 .requestMatchers("/aluno/**").hasAuthority("ALUNO")
                 .requestMatchers("/cordenador/**").hasAuthority("CORDENADOR")
-
-                // Qualquer outra requisição precisa de autenticação
                 .anyRequest().authenticated()
             )
-            // Filtro JWT antes do UsernamePasswordAuthenticationFilter
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
