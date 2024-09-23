94fdeaf86d7e29f9ed6ef77a92470c9ec6815990
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2024-09-23 10:47:36 -0300

    Update UserService.java

--- src/main/java/com/agendasenac/services/UserService.java
+++ src/main/java/com/agendasenac/services/UserService.java
@@ -2,6 +2,7 @@ package com.agendasenac.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.agendasenac.util.SecurityConfig;
 import org.springframework.stereotype.Service;
 
 import com.agendasenac.modells.UserSistema;
@@ -9,6 +10,9 @@ import com.agendasenac.repository.UserSistemaRepository;
 
 @Service
 public class UserService {
+    
+    @Autowired
+    private SecurityConfig securityconfig;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -18,7 +22,7 @@ public class UserService {
 
     public UserSistema criarUser(UserSistema user) {
         // Codificar a senha antes de salvar
-        user.setSenhaAcessoUser(passwordEncoder.encode(user.getSenhaAcessoUser()));
+        user.setSenhaAcessoUser(securityconfig.passwordEncoder.encode(user.getSenhaAcessoUser()));
         return userRepository.save(user);
     }
 }
