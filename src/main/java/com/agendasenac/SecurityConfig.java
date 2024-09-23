58675fff6cc690c07d3654f9eabf9bebdb4e9526
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2024-09-23 10:36:57 -0300

    Update SecurityConfig.java

--- src/main/java/com/agendasenac/SecurityConfig.java
+++ src/main/java/com/agendasenac/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.stereotype.Component;
 import com.agendasenac.componets.JwtAuthenticationFilter;
 
-@Component
+@Configuration
 public class SecurityConfig {
 
     @Bean
