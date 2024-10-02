caac6a68219ff4980de57a38eefe37a33d0ceec3
B2XGa <B2XGa@RioMarRecife2>
2024-10-02 16:08:25 -0300

    sem nome do professor porquerrrrrr:::^^^}

--- src/main/java/com/agendasenac/modells/Disciplinas.java
+++ src/main/java/com/agendasenac/modells/Disciplinas.java
@@ -36,13 +36,17 @@ public class Disciplinas implements Serializable{
 	@ManyToOne(optional = true)
 	private UserSistema professor;
 	
+	
+	public String getnomeprofessor() {
+        return professor != null ? professor.getNomeCompletoUser() : "SEM NOME"; ///pq tu não quer irrr
+    }
+
+    public String getcontatoprofessor() {
+        return professor != null ? professor.getContatopessoal() : "SEM CONTATO";
+    }
     
     public Long getprovessorid() {
     	return professor != null ? professor.getCodigo() : null;
-    }
-    
-    public String getprofessor() {
-        return professor != null ? professor.getNomeCompletoUser() : "SEM NOME"; ///pq tu não quer irrr
     }
 
 	public void setProfessor(UserSistema professor) {
