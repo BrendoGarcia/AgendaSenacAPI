f1e00c75fe6afa620931917539328ea4299d389a
B2XGa <B2XGa@RioMarRecife2>
2024-10-05 20:55:13 -0300

    desfazendo

--- src/main/java/com/agendasenac/controllers/LoginController.java
+++ src/main/java/com/agendasenac/controllers/LoginController.java
@@ -1,67 +1,53 @@
+package com.agendasenac.controllers;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder; // Adicionar o encoder para comparar a senha
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agendasenac.modells.JwtResponse;
 import com.agendasenac.modells.LoginRequest;
-import com.agendasenac.modells.UserSistema;
-import com.agendasenac.repository.UserSistemaRepository;
 import com.agendasenac.services.AuthenticationService;
+import com.agendasenac.util.CodigoUtilRapido;
 
 @RestController
 public class LoginController {
-
-    @Autowired
+	
+	@Autowired
     private AuthenticationService authenticationService;
+	
+	
+	@PostMapping("/login")
+	@CrossOrigin
+	public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest authRequest) {
+	        try {
+	            // Autentica o usuário e gera o token
+	            String token = authenticationService.authenticate(authRequest.getUserEmail(), authRequest.getUserSenha());
+	            
+	            Map<String, String> response = new HashMap<>();
+		        response.put("Token ",  token);
+	            
+	            return ResponseEntity.ok(response);
+	        } catch (AuthenticationException e) {
+	            // Se houver erro de autenticação, retorne 401 Unauthorized
+	        	Map<String, String> response = new HashMap<>();
+		        response.put("Aceso ",  "Negado");
+	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+	        }
+	}
 
-    @Autowired
-    private UserSistemaRepository usersistema;
+		
+		
+		
 
-    @Autowired
-    private PasswordEncoder passwordEncoder; // Adiciona o PasswordEncoder
-
-    @PostMapping("/login")
-    @CrossOrigin
-    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest authRequest) {
-        try {
-            // Autentica o usuário e gera o token
-            String token = authenticationService.authenticate(authRequest.getUserEmail(), authRequest.getUserSenha());
-
-            // Busca o usuário no banco de dados apenas pelo e-mail
-            Optional<UserSistema> optionalUsuario = usersistema.findByimailUser(authRequest.getUserEmail());
-
-            Map<String, Object> response = new HashMap<>();
-            response.put("Token", token);
-
-            // Verifica se o usuário foi encontrado
-            if (optionalUsuario.isPresent()) {
-                UserSistema usuario = optionalUsuario.get();
-
-                // Compara a senha não codificada com a senha armazenada (hash)
-                if (passwordEncoder.matches(authRequest.getUserSenha(), usuario.getSenhaAcessoUser())) {
-                    response.put("DadosUser", usuario);
-                } else {
-                    response.put("DadosUser", "Senha incorreta");
-                }
-            } else {
-                response.put("DadosUser", "Usuário não encontrado");
-            }
-
-            return ResponseEntity.ok(response);
-        } catch (AuthenticationException e) {
-            // Se houver erro de autenticação, retorne 401 Unauthorized
-            Map<String, Object> response = new HashMap<>();
-            response.put("Acesso", "Negado");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-        }
-    }
 }
