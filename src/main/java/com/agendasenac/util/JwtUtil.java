52e6692f91999fdad7568e52365ad40d14abbc1b
B2XGa <B2XGa@RioMarRecife2>
2024-10-05 14:56:13 -0300

    sei la o que to tentnadoa

--- src/main/java/com/agendasenac/util/JwtUtil.java
+++ src/main/java/com/agendasenac/util/JwtUtil.java
@@ -57,6 +57,7 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
+        		.addClaims(claims)
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
