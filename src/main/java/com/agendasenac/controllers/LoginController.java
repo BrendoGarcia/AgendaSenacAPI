4bcf74efea3889f56b4fea1552c5fc7e8b2dc5ee
B2XGa <B2XGa@RioMarRecife2>
2024-10-05 21:26:29 -0300

    teste

--- src/main/java/com/agendasenac/controllers/LoginController.java
+++ src/main/java/com/agendasenac/controllers/LoginController.java
@@ -45,8 +45,8 @@ public class LoginController {
                 // Cria uma string com os dados do usuário que você deseja retornar
                 String Nome = usuario.getNomeCompletoUser();
                 RegraUsers Tipo = usuario.getTipoUser();
-                response.put("NomeUsuario", Nome + Tipo); 
-                response.put("TipoUser" + Tipo, "");
+                response.put("NomeUsuario", Nome); 
+                response.put("TipoUser", "" + Tipo );
             } else {
                 response.put("DadosUser", "Usuário não encontrado"); // Mensagem se não encontrar
             }
