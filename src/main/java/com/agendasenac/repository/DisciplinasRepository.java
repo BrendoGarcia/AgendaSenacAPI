505401fa7d225697be1e06b2164807a0e8095415
B2XGa <B2XGa@RioMarRecife2>
2024-10-09 20:45:32 -0300

    segue essa bomba

--- src/main/java/com/agendasenac/repository/DisciplinasRepository.java
+++ src/main/java/com/agendasenac/repository/DisciplinasRepository.java
@@ -2,13 +2,16 @@ package com.agendasenac.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.agendasenac.modells.Disciplinas;
 
 public interface DisciplinasRepository extends CrudRepository<Disciplinas, Long>{
 			Disciplinas findByidDisciplina(Long idDisciplina);
 			List<Disciplinas> findByProfessorCodigo(Long codigo);
-			List<Disciplinas> findByProfessorCodigoTurma(Long codigo, Long turma);
+			@Query("SELECT d FROM Disciplinas d WHERE d.professor.codigo = :codigo AND d.turma.idturma = :idturma")
+		    List<Disciplinas> findByProfessorCodigoTurma(@Param("codigo") Long codigo, @Param("idturma") Long idturma);
 			
 }			
