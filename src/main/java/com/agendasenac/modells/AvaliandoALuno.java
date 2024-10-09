4863c59bb6e0d68e6eae60d7161e2f2625fad7d0
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2024-10-09 11:07:23 -0300

    arrumando exbao avaliacion

--- src/main/java/com/agendasenac/modells/AvaliandoALuno.java
+++ src/main/java/com/agendasenac/modells/AvaliandoALuno.java
@@ -45,17 +45,31 @@ public class AvaliandoALuno implements Serializable{
 		this.idavalicacion = idavalicacion;
 	}
 
-	public UserSistema getAluno() {
-		return aluno;
-	}
+	//
+	
+	public String getnomeAluno() {
+        return aluno != null ? aluno.getNomeCompletoUser() : "SEM NOME"; ///pq tu não quer irrr
+    }
+	
+	public Long getidALuno() {
+        return aluno != null ? aluno.getCodigo() : null; ///pq tu não quer irrr
+    }
+	
+	
+	public Long getidprofessor() {
+        return Professor != null ? Professor.getCodigo() : null; ///pq tu não quer irrr
+    }
+	
+	public String getnomer() {
+        return Professor != null ? Professor.getNomeCompletoUser() : null; ///pq tu não quer irrr
+    }
+	
 
+	//
 	public void setAluno(UserSistema aluno) {
 		this.aluno = aluno;
 	}
 
-	public UserSistema getProfessor() {
-		return Professor;
-	}
 
 	public void setProfessor(UserSistema professor) {
 		Professor = professor;
